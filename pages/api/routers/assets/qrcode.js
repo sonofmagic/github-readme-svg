8ccabce9a42f541c69385390394d4f37c02d5ad7
sonofmagic <1324318532@qq.com>
2022-08-07 02:23:43 +0800

    feat: revert @icebreakers/qrcode

--- pages/api/routers/assets/qrcode.js
+++ pages/api/routers/assets/qrcode.js
@@ -1,33 +1,33 @@
 const QRCode = require('qrcode-svg')
-const IceQRCode = require('@icebreakers/qrcode')
+// const IceQRCode = require('@icebreakers/qrcode')
 
 function renderQRCode(params) {
   const { value, p, w, h, size, color, background, ecl, type, ...args } = params
-  if (type) {
-    const fn = IceQRCode[type]
-    if (typeof fn === 'function') {
-      const svg = fn({
-        content: value,
-        size: size || w || h,
-        level: ecl,
-        type: args.qrcodeType,
-        ...args,
-      })
-      return svg
-    }
-  } else {
-    const qrcodeSvg = new QRCode({
-      content: value,
-      padding: p,
-      width: w || size,
-      height: h || size,
-      color,
-      background,
-      ecl,
-      join: true,
-    }).svg()
-    return qrcodeSvg
-  }
+  // if (type) {
+  //   const fn = IceQRCode[type]
+  //   if (typeof fn === 'function') {
+  //     const svg = fn({
+  //       content: value,
+  //       size: size || w || h,
+  //       level: ecl,
+  //       type: args.qrcodeType,
+  //       ...args,
+  //     })
+  //     return svg
+  //   }
+  // } else {
+  const qrcodeSvg = new QRCode({
+    content: value,
+    padding: p,
+    width: w || size,
+    height: h || size,
+    color,
+    background,
+    ecl,
+    join: true,
+  }).svg()
+  return qrcodeSvg
+  // }
 }
 
 module.exports = {
